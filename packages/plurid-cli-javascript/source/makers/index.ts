accf8fbb4f176c3ebe332244c2ac8db6d9520cab
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-12-28 22:19:01 +0200

    plurid-cli-javascript > dev: makers

--- packages/plurid-cli-javascript/source/makers/index.ts
+++ packages/plurid-cli-javascript/source/makers/index.ts
@@ -2,7 +2,7 @@
     // #region internal
     import makeApplicationCommand from './application';
     import makeCloudCommand from './cloud';
-    import makeProductsCommand from './cloud';
+    import makeProductsCommand from './products';
     // #endregion internal
 // #endregion imports
 
